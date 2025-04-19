36ad909115bc85aa4a7650c3c6b25b8c6ea1350d
Plow Miner <parag@rigai.co>
2025-04-18 23:13:14 -0700

    Committing some image renames

--- Utility.py
+++ Utility.py
@@ -1,1 +1,20 @@
 # From the folders, we can use the images to create sprite sheets.
+import os
+import re
+
+# Path to the Images folder
+images_folder = "Images"
+
+# Iterate through each folder inside the Images folder
+for folder_name in os.listdir(images_folder):
+    folder_path = os.path.join(images_folder, folder_name)
+    
+    # Check if it's a directory
+    if os.path.isdir(folder_path):
+        # Remove numbers and underscores from the folder name
+        new_folder_name = re.sub(r'[0-9_]', '', folder_name)
+        new_folder_path = os.path.join(images_folder, new_folder_name)
+        
+        # Rename the folder
+        os.rename(folder_path, new_folder_path)
+        print(f"Renamed: {folder_name} -> {new_folder_name}")
